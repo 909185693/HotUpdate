352ac98e9238b84403deb6ab9ed64d6ef3a8c6ee
山城老司机 <909185693@qq.com>
2024-01-22 21:53:14 +0800

    编辑器下不执行版本捕获脚本

--- Source/HotUpdate/HotUpdate.Build.cs
+++ Source/HotUpdate/HotUpdate.Build.cs
@@ -18,77 +18,80 @@ public class HotUpdate : ModuleRules
 
         OptimizeCode = CodeOptimization.InShippingBuildsOnly;
 
+        if (!Target.bBuildEditor)
+        {
 #if !__MonoCS__
-        var ProjectDir = Target.ProjectFile.Directory;
-        var ConfigFilePath = ProjectDir + "/Config/DefaultHotUpdate.ini";
-        var ConfigFileReference = new FileReference(ConfigFilePath);
-        var ConfigFile = FileReference.Exists(ConfigFileReference) ? new ConfigFile(ConfigFileReference) : new ConfigFile();
-        var Config = new ConfigHierarchy(new[] { ConfigFile });
-        const string Section = "/Script/HotUpdate.HotUpdateSettings";
+            var ProjectDir = Target.ProjectFile.Directory;
+            var ConfigFilePath = ProjectDir + "/Config/DefaultHotUpdate.ini";
+            var ConfigFileReference = new FileReference(ConfigFilePath);
+            var ConfigFile = FileReference.Exists(ConfigFileReference) ? new ConfigFile(ConfigFileReference) : new ConfigFile();
+            var Config = new ConfigHierarchy(new[] { ConfigFile });
+            const string Section = "/Script/HotUpdate.HotUpdateSettings";
 
-        int MajorVersion = 0;
-        Config.GetInt32(Section, "MajorVersion", out MajorVersion);
+            int MajorVersion = 0;
+            Config.GetInt32(Section, "MajorVersion", out MajorVersion);
 
-        int MinorVersion = 0;
-        Config.GetInt32(Section, "MinorVersion", out MinorVersion);
+            int MinorVersion = 0;
+            Config.GetInt32(Section, "MinorVersion", out MinorVersion);
 
-        string Arguments = MajorVersion + " " + MinorVersion + " ";
+            string Arguments = MajorVersion + " " + MinorVersion + " ";
 
-        string VersionController;
-        Config.GetString(Section, "VersionController", out VersionController);
+            string VersionController;
+            Config.GetString(Section, "VersionController", out VersionController);
 
-        string BatchDirectory = null;
-        if (VersionController == "Git")
-        {
-            BatchDirectory = Path.Combine(PluginDirectory, "Batch/Git");
-        }
-        else if (VersionController == "Perforce")
-        {
-            BatchDirectory = Path.Combine(PluginDirectory, "Batch/Perforce");
-
-            string P4Server;
-            Config.GetString(Section, "P4Server", out P4Server);
-
-            string P4Username;
-            Config.GetString(Section, "P4Username", out P4Username);
-
-            string P4Password;
-            Config.GetString(Section, "P4Password", out P4Password);
-
-            string P4Workspaces;
-            Config.GetString(Section, "P4Workspaces", out P4Workspaces);
-
-            Arguments += P4Server + " " + P4Username + " " + P4Password + " " + P4Workspaces + " ";
-        }
-        else if (VersionController == "Subversion")
-        {
-            BatchDirectory = Path.Combine(PluginDirectory, "Batch/Subversion");
-        }
-
-        if (BatchDirectory != null)
-        {
-            System.Console.WriteLine("-------------------- HotUpdate Config ---------------------");
-            Process Proc = new Process();
-            Proc.StartInfo.WorkingDirectory = BatchDirectory;
-            Proc.StartInfo.FileName = Path.Combine(BatchDirectory, "Version.bat");
-            Proc.StartInfo.Arguments = Arguments;
-            Proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            Proc.StartInfo.RedirectStandardOutput = true;
-            Proc.StartInfo.RedirectStandardError = true;
-            Proc.StartInfo.UseShellExecute = false;
-            Proc.OutputDataReceived += new DataReceivedEventHandler((SendingProcess, OutLine) =>
+            string BatchDirectory = null;
+            if (VersionController == "Git")
             {
-                System.Console.WriteLine(OutLine.Data);
-            });
-            Proc.Start();
-            Proc.BeginOutputReadLine();
-            Proc.BeginErrorReadLine();
-            Proc.WaitForExit();
+                BatchDirectory = Path.Combine(PluginDirectory, "Batch/Git");
+            }
+            else if (VersionController == "Perforce")
+            {
+                BatchDirectory = Path.Combine(PluginDirectory, "Batch/Perforce");
 
-            System.Console.WriteLine("Auto write version file by " + VersionController);
-            System.Console.WriteLine("---------------------------------------------------------------");
-        }
+                string P4Server;
+                Config.GetString(Section, "P4Server", out P4Server);
+
+                string P4Username;
+                Config.GetString(Section, "P4Username", out P4Username);
+
+                string P4Password;
+                Config.GetString(Section, "P4Password", out P4Password);
+
+                string P4Workspaces;
+                Config.GetString(Section, "P4Workspaces", out P4Workspaces);
+
+                Arguments += P4Server + " " + P4Username + " " + P4Password + " " + P4Workspaces + " ";
+            }
+            else if (VersionController == "Subversion")
+            {
+                BatchDirectory = Path.Combine(PluginDirectory, "Batch/Subversion");
+            }
+
+            if (BatchDirectory != null)
+            {
+                System.Console.WriteLine("-------------------- HotUpdate Config ---------------------");
+                Process Proc = new Process();
+                Proc.StartInfo.WorkingDirectory = BatchDirectory;
+                Proc.StartInfo.FileName = Path.Combine(BatchDirectory, "Version.bat");
+                Proc.StartInfo.Arguments = Arguments;
+                Proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                Proc.StartInfo.RedirectStandardOutput = true;
+                Proc.StartInfo.RedirectStandardError = true;
+                Proc.StartInfo.UseShellExecute = false;
+                Proc.OutputDataReceived += new DataReceivedEventHandler((SendingProcess, OutLine) =>
+                {
+                    System.Console.WriteLine(OutLine.Data);
+                });
+                Proc.Start();
+                Proc.BeginOutputReadLine();
+                Proc.BeginErrorReadLine();
+                Proc.WaitForExit();
+
+                System.Console.WriteLine("Auto write version file by " + VersionController);
+                System.Console.WriteLine("---------------------------------------------------------------");
+            }
 #endif
+        }
 
         PublicIncludePaths.AddRange(
             new string[]
